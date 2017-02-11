5186a2af2e5c1114af632f8004313c56a032c255
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-02-11 08:38:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,14 +3,12 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
 
-function moveLeft(){
-    marginRight=0;
-    marginRight+=10;
-    img.style.marginRight = marginRight + 'px';
-}
-
 var img = document.getElementById('modi');
+marginLeft = 0;
+function moveRight(){
+    marginLeft += 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function(){
-    var interval= setInterval(moveLeft,100)
-    img.style.marginRight = '100px';
+    var interval= setInterval(moveRight,100);
 };
