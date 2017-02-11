1c77fe68639f61bf50ec66d3edaedf4b69a5470c
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-02-11 08:25:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML='new value';
+
+var img = document.getElementById('modi');
+img.onclick = function(){
+    img.style.marginRight = '100px';
+};
