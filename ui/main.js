6d7e95a1bc95f849d0fe3a80c50630f9a4145898
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-02-11 10:05:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,24 @@
-console.log('Loaded!');
+// //counter code
 
-// var element = document.getElementById('main-text');
-// element.innerHTML = 'new value';
-
-var img = document.getElementById('modi');
-marginLeft = 0;
-function moveRight(){
-    marginLeft += 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval= setInterval(moveRight,50);
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    counter+=1;
+    var span = document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
+
+// console.log('Loaded!');
+
+// // var element = document.getElementById('main-text');
+// // element.innerHTML = 'new value';
+
+// var img = document.getElementById('modi');
+// marginLeft = 0;
+// function moveRight(){
+//     marginLeft += 1;
+//     img.style.marginLeft = marginLeft + 'px';
+// }
+// img.onclick = function(){
+//     var interval= setInterval(moveRight,50);
+// };
