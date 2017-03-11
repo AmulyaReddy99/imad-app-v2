321596dafcab6388b5912b5ce0290c26538d7190
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-11 18:58:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 var Pool = require('pg').Pool;
 
 var config = {
-    user: 'AmulyaReddy99',
-    database: 'AmulyaReddy99',
+    user: 'amulyareddy99',
+    database: 'amulyareddy99',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
