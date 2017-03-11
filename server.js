2f076eaecd477148568e8f7bc0963c15bb519a32
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-11 18:31:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
 var app = express();
+
+var config = {
+    user: 'AmulyaReddy99';
+    database: 'AmulyaReddy99';
+    host: 'db.imad.hasura-app.io'
+    port: '5432';
+    password: process.env.DB_PASSWORD
+}
 app.use(morgan('combined'));
 
 var articles={
@@ -79,6 +87,18 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+    //make select req n respond to results 
+    pool.query('Select * FROM test',function(err,result)){
+        if(err){
+            res.status(500).send(err.toString());
+        }   else {
+            res.send(JSON.stringify(result));
+        }
+    }
 });
 
 var counter=0;
