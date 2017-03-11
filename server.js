f20b2573ea9563d07194695a9d4408aa055d9948
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-11 19:25:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/article/:articleName',function(req,res){
    // var articleName = req.params.articleName;
     
   //  var articleName =  req.params.articleName;
-   pool.query("SELECT * FROM article WHERE title" + articleName, function(err,result){
+   pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result){
        if(err){
            res.status(500).send(err.toString());
        }    else{
