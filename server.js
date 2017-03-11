97b88dac1ff4ac5f7bef0e58894bf2e6f7c6116a
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-11 18:44:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var app = express();
 
-var config = {
-    user: 'AmulyaReddy99';
-    database: 'AmulyaReddy99';
-    host: 'db.imad.hasura-app.io'
-    port: '5432';
-    password: process.env.DB_PASSWORD
-}
+// var config = {
+//     user: 'AmulyaReddy99';
+//     database: 'AmulyaReddy99';
+//     host: 'db.imad.hasura-app.io'
+//     port: '5432';
+//     password: process.env.DB_PASSWORD
+// }
 app.use(morgan('combined'));
 
 var articles={
@@ -89,17 +89,17 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(config);
-app.get('/test-db', function (req, res) {
-    //make select req n respond to results 
-    pool.query('Select * FROM test',function(err,result)){
-        if(err){
-            res.status(500).send(err.toString());
-        }   else {
-            res.send(JSON.stringify(result));
-        }
-    }
-});
+// var pool = new Pool(config);
+// app.get('/test-db', function (req, res) {
+//     //make select req n respond to results 
+//     pool.query('Select * FROM test',function(err,result)){
+//         if(err){
+//             res.status(500).send(err.toString());
+//         }   else {
+//             res.send(JSON.stringify(result));
+//         }
+//     }
+// });
 
 var counter=0;
 app.get('/counter',function(req,res){
