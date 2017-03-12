28dacecff8760bf5a8ed19b46f81be78e7b1cad5
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-12 08:19:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,33 +1,33 @@
-// // //counter code
-
-// var button = document.getElementById('counter');
-
-// button.onclick = function(){
-//     //create request to the counter endpoint
-//     var request = new XMLHttpRequest();
-    
-//     //capture the response and store the response in a variable
-//     reqest.onreadystatechange = function(){
-//         if(request.readyState === XMLHttpRequest.DONE){
-//             if(request.status === 200){
-//                 var counter = request.resposeText;
-//                 var span = document.getElementById('count');
-//                 span.innerHTML=counter.toString();
-//             }
-//         } 
-//     };
-//     //make the request
-//     request.open('GET','http://amulyareddy99.imad.hasura-app.io/counter',true);
-//     request.send(null);
-// };
+// //counter code
 
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //create request to the counter endpoint
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store the response in a variable
+    reqest.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        } 
+    };
+    //make the request
+    request.open('GET','http://amulyareddy99.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
+
+// var button = document.getElementById('counter');
+// var counter = 0;
+// button.onclick = function(){
+//     counter = counter + 1;
+//     var span = document.getElementById('count');
+//     span.innerHTML = counter.toString();
+// };
 
 
 // //submit name
