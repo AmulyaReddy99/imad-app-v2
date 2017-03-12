43f0a31f56c68540778f818a65b423697a9fabb0
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-12 21:00:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,22 +122,38 @@ app.get('/submit-name/:name', function (req, res) {
     res.send(JSON.stringify(names));
 }); 
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
 app.get('/:articleName',function(req,res){
-  // var articleName = req.params.articleName;
-    
-  //  var articleName =  req.params.articleName;
-  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result){
-      if(err){
-          res.status(500).send(err.toString());
-      }    else{
-          if(result.rows.length === 0){
-              res.status(404).send('Article not found');
-          }  else{
-                var articleData = result.rows[0];
-                res.send(createTemplate(articlesData));
-          }
-      }
-  });
+   var articleName = req.params.articleName;
+   res.send(createTemplaate(articles[articleName]));
+   
+//   pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result){
+//       if(err){
+//           res.status(500).send(err.toString());
+//       }    else{
+//           if(result.rows.length === 0){
+//               res.status(404).send('Article not found');
+//           }  else{
+//                 var articleData = result.rows[0];
+//                 res.send(createTemplate(articlesData));
+//           }
+//       }
+//   });
 });
 
 // app.get('/article-one',function(req,res){
@@ -155,19 +171,4 @@ app.get('/:articleName',function(req,res){
 // res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 // });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
