4ee6706c432074d6b6040cb9cd17af6883eef82b
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-12 21:01:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,10 +134,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
 app.get('/:articleName',function(req,res){
    var articleName = req.params.articleName;
    res.send(createTemplaate(articles[articleName]));
@@ -154,6 +150,11 @@ app.get('/:articleName',function(req,res){
 //           }
 //       }
 //   });
+});
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
 });
 
 // app.get('/article-one',function(req,res){
