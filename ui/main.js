86c170e334e566dbee3976a3b43941067f2b601b
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-12 18:23:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,14 +10,19 @@ button.onclick = function(){
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML=counter.toString();
-            }
-        } 
-    };
+                    var names = request.responseText;
+                    names = JSON.parse(names);
+                    var list='';
+                    for(var i=0;i<names.length;i++){
+                      list+= '<li>' + names[i] + '</li>';
+                    }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
+                    }
+                } 
+            };
     //make the request
-    request.open('GET','http://amulyareddy99.imad.hasura-app.io/counter',true);
+    request.open('GET','http://amulyareddy99.imad.hasura-app.io/submit-name?='+ name ,true);
     request.send(null);
 };
 
@@ -26,14 +31,8 @@ var nameInput = document.getElementById('name');
 var name = nameInput.vlaue; 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-      list+= '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-}
+ 
+};
 
 // var button = document.getElementById('counter');
 // var counter = 0;
