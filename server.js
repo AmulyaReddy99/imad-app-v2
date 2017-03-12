fe340c472f778ff45b4cfb02f53a95f83bff17ab
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-12 17:58:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,8 +158,10 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function (req, res) {
-    var name = req.params.name;
+//app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name/:name', function (req, res) { 
+    var name = req.query.name;
+//    var name = req.params.name;
     names.push(name);
     
     res.send(JSON.stringify(names));
