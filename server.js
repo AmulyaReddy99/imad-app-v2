fa8e12b5a99dcc023bc41fd127d0e29d9d22a303
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-12 21:13:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/submit-name/:name', function (req, res) {
 app.get('/:articleName',function(req,res){
    var articleName = req.params.articleName;
    res.send(createTemplaate(articles[articleName]));
-
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
