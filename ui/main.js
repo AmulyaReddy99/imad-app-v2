d86a379156e0df618349bd45434e5101f15d12b3
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-12 08:01:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,39 +1,46 @@
-// //counter code
+// // //counter code
+
+// var button = document.getElementById('counter');
+
+// button.onclick = function(){
+//     //create request to the counter endpoint
+//     var request = new XMLHttpRequest();
+    
+//     //capture the response and store the response in a variable
+//     reqest.onreadystatechange = function(){
+//         if(request.readyState === XMLHttpRequest.DONE){
+//             if(request.status === 200){
+//                 var counter = request.resposeText;
+//                 var span = document.getElementById('count');
+//                 span.innerHTML=counter.toString();
+//             }
+//         } 
+//     };
+//     //make the request
+//     request.open('GET','http://amulyareddy99.imad.hasura-app.io/counter',true);
+//     request.send(null);
+// };
 
 var button = document.getElementById('counter');
-
-button.onclick = function(){
-    //create request to the counter endpoint
-    var request = new XMLHttpRequest();
-    
-    //capture the response and store the response in a variable
-    reqest.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var counter = request.resposeText;
-                var span = document.getElementById('count');
-                span.innerHTML=counter.toString();
-            }
-        } 
-    };
-    //make the request
-    request.open('GET','http://amulyareddy99.imad.hasura-app.io/counter',true);
-    request.send(null);
+var counter = 0;
+button.onClick = function(){
+    counter = counter + 1;
+    var span = documente.getElementById('count');
+    span.innerHTML = counter.toString();
 };
-
-//submit name
-var nameInput=document.getElementById('name');
-var name=nameInput.vlaue; 
-var submit=document.getElementById('submit_btn');
-submit.onclick = function(){
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-       list+= '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ui.innerHTML = list;
-}
+// //submit name
+// var nameInput=document.getElementById('name');
+// var name=nameInput.vlaue; 
+// var submit=document.getElementById('submit_btn');
+// submit.onclick = function(){
+//     var names=['name1','name2','name3','name4'];
+//     var list='';
+//     for(var i=0;i<names.length;i++){
+//       list+= '<li>' + names[i] + '</li>';
+//     }
+//     var ul = document.getElementById('namelist');
+//     ui.innerHTML = list;
+// }
 
 
 // console.log('Loaded!');
