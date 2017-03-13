f514d926be30a22a680faf5113a57234f4bc9ae1
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-13 18:51:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,19 +122,6 @@ app.get('/submit-name/:name', function (req, res) {
     res.send(JSON.stringify(names));
 }); 
 
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
 app.get('/article/:articleName',function(req,res){
 //   var articleName = req.params.articleName;
 //   res.send(createTemplaate(articles[articleName]));
@@ -151,6 +138,18 @@ app.get('/article/:articleName',function(req,res){
           }
       }
   });
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
