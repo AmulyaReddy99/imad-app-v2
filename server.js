c5a6b5eabe552ac3437186b5fbc56cb10d6f748e
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-13 19:01:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,10 +131,10 @@ app.get('/articles/:articleName',function(req,res){
           res.status(500).send(err.toString());
       }    else{
           if(result.rows.length === 0){
-              res.status(404).send('Article not found ABC');
+              res.status(404).send('Article not found');
           }  else{
                 var articleData = result.rows[0];
-                res.send(createTemplate(articlesData));
+                res.send(createTemplate(articleData));
           }
       }
   });
