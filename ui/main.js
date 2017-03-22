4f5a06db03e598e6a28d457e034635fb861f9458
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-22 17:31:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,12 @@ var nameInput = document.getElementById('name');
 var name = nameInput.vlaue; 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
- 
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0; i<names.length ; i++){
+        list+='<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
 };
 
 // var button = document.getElementById('counter');
