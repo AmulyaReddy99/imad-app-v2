6bc079d18f1039d66c945addfa2acff647fa449f
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-24 18:48:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -164,20 +164,3 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-
-// app.get('/article-one',function(req,res){
-// res.send(createTemplate(article-one));
-// });
-
-// app.get('/article-two',function(req,res){
-// res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-// });
-
-// app.get('/article-two',function(req,res){
-//     res.send('article-two is requested and will be served here');
-// });
-// app.get('/article-three',function(req,res){
-// res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-// });
-
-
