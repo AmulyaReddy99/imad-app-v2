fbdb5a86d2deefc52beef140033dfe3a6f463aa0
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-24 05:43:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-//counter code
+//username and password
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
@@ -9,28 +9,25 @@ submit.onclick = function(){
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                    var names = request.responseText;
-                    names = JSON.parse(names);
-                    var list='';
-                    for(var i=0;i<names.length;i++){
-                      list += '<li>' + names[i] + '</li>';
-                    }
-                    var ul = document.getElementById('namelist');
-                    ul.innerHTML = list;
+                    
                     }
                 } 
             };
     //make the request
-    var nameInput = document.getElementById('name');
+    var username = document.getElementById('username');
+    var password = document.getElementById('password');
+    console.log(username);
+    console.log(password);
     var name = nameInput.vlaue; 
-    request.open('GET','http://amulyareddy99.imad.hasura-app.io/submit-name?name='+ name,true);
-    request.send(null);
+    request.open('POST','http://amulyareddy99.imad.hasura-app.io/login',true);
+    request.send(JSON.stringify({username:"username", password:"password"}));
     
 };
 
 //submit name
 // var submit = document.getElementById('submit_btn');
 // submit.onclick = function(){
+//---------------------------------------------------------
 //     var names = request.responseText;
 //     names = JSON.parse(names);
 //     var list = '';
@@ -38,6 +35,8 @@ submit.onclick = function(){
 //         list+='<li>' + names[i] + '</li>';
 //     }
 //     var ul = document.getElementById('namelist');
+//     ul.innerHTML = list;
+//---------------------------------------------------------
 //     var nameInput = document.getElementById('name');
 //     var name = nameInput.vlaue; 
 //     request.open('GET','http://amulyareddy99.imad.hasura-app.io/submit-name?name='+ name,true);
