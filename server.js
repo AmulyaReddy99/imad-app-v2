911cc6bba2ba4d8e339a82b7b838b757d2552995
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-24 18:54:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,27 +81,6 @@ app.post('/create-user',function(rq,res){
         }  
     });
 });
-app.post('/login',function(rq,res){
-    var username = req.body.username;
-    var password = req.body.password;
-    pool.query('SELECT * FROM "user" WHERE username = $1', [username] ,function(err,result){
-       if(err){
-            res.status(500).send(err.toString());
-        } else { if(result.rows.length === 0) 
-                 res.send(403).send('username/password is invalid ');
-        else {
-            var dbString = result.row[0].password;
-            var salt = dbString.split('$')[2];
-            var hashedPassword = hash(password,salt);
-            if(hashedPassword === dbString){
-                res.send('credentials are correct! ');
-            } else {
-                res.send(403).send('username/password is invalid ');
-            }
-          }
-        }
-    });
-});
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
