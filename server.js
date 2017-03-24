50b033c4c9cb8a3a96a4998446ff7c13b0fcc642
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-03-24 18:51:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,24 +68,24 @@ app.get('/hash/:input', function (req, res){
     res.send(hashedString);
 });
 
-app.post('/create-user',function(req,res){
+app.post('/create-user',function(rq,res){
     var username = req.body.username;
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
     pool.query('INSERT INTO "user" (username, password) VALUES ($1,$2)', [username,dbString] ,function(err,result){
-      if(err){
+       if(err){
             res.status(500).send(err.toString());
         }   else {
             res.send('user sucessfully created: '+ username);
         }  
     });
 });
-app.post('/login',function(req,res){
+app.post('/login',function(rq,res){
     var username = req.body.username;
     var password = req.body.password;
     pool.query('SELECT * FROM "user" WHERE username = $1', [username] ,function(err,result){
-      if(err){
+       if(err){
             res.status(500).send(err.toString());
         } else { if(result.rows.length === 0) 
                  res.send(403).send('username/password is invalid ');
